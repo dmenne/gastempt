cae388e5c8269fa9911df0f8435d355837277041
Dieter Menne <dieter.menne@menne-biomed.de>
2016-05-05 11:10:06 +0200

    Modified stan test to be skipped on travis

--- tests/testthat/test-stan.R
+++ tests/testthat/test-stan.R
@@ -18,36 +18,37 @@ test_that("stanmodels$linexp_gastro_1b exists", {
   expect_silent(stanmodels$linexp_gastro_1b)
 })
 
-test_that("Basic direct use of Stan returns valid results", {
-  set.seed(471)
-  s = simulate_gastempt(n_records = 6)
-  mr = s$data %>%
-    mutate(
-      record_i = as.integer(as.factor(record))
-    )
-  prior_v0 = mr %>%  filter(minute == 0) %>%
-    summarize(
-      v0 = median(vol)
-    )
-  n_record = max(mr$record_i)
-  rstan_options(auto_write = TRUE)
-  options(mc.cores = parallel::detectCores())
+skip_on_travis(
+  test_that("Basic direct use of Stan returns valid results", {
+    set.seed(471)
+    s = simulate_gastempt(n_records = 6)
+    mr = s$data %>%
+      mutate(
+        record_i = as.integer(as.factor(record))
+      )
+    prior_v0 = mr %>%  filter(minute == 0) %>%
+      summarize(
+        v0 = median(vol)
+      )
+    n_record = max(mr$record_i)
+    rstan_options(auto_write = TRUE)
+    options(mc.cores = parallel::detectCores())
 
 
-  data = list(
-    prior_v0 = unlist(prior_v0$v0),
-    n = nrow(mr),
-    n_record = n_record,
-    record = mr$record_i,
-    minute = mr$minute,
-    volume = mr$vol)
-  model = stanmodels$linexp_gastro_1b
-  mr_stan <- sampling(model,
-       chains = 1, iter = 1000, data = data, seed = 4711)
-  expect_s4_class(mr_stan, "stanfit")
+    data = list(
+      prior_v0 = unlist(prior_v0$v0),
+      n = nrow(mr),
+      n_record = n_record,
+      record = mr$record_i,
+      minute = mr$minute,
+      volume = mr$vol)
+    model = stanmodels$linexp_gastro_1b
+    expect_
+    mr_stan <- sampling(model, chains = 1, iter = 1000, data = data)
+    expect_s4_class(mr_stan, "stanfit")
 
-  ss  = summary(mr_stan, "v0")$summary[,1]
-  # residual standard deviation
-  expect_lt(sqrt(var(ss - s$record$v0)), 7)
-})
-
+    ss  = summary(mr_stan, "v0")$summary[,1]
+    # residual standard deviation
+    expect_lt(sqrt(var(ss - s$record$v0)), 7)
+  })
+)
