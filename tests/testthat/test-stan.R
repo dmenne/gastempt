1a6b5d4d1755ea01edad7566d1d244fe5eb6b4fb
Dieter Menne <dieter.menne@menne-biomed.de>
2017-05-10 08:30:40 +0200

    nlme in single quotes in DESCRIPTION

--- tests/testthat/test-stan.R
+++ tests/testthat/test-stan.R
@@ -27,42 +27,8 @@ gastempt_data = function(){
   simulate_gastempt(n_records = 6, seed = 471)$stan_data
 }
 
-if (FALSE)
-test_that("Direct use of sample model returns valid results", {
-  # This test must compile the model and is slow.
-  # Only use on errors in other Stan functions.
-  data = gastempt_data()
-  stan_model = "../../exec/linexp_gastro_2b.stan"
-  expect_true(file.exists(stan_model))
-  rstan_options(auto_write = TRUE)
-  iter = 500
-  cap = capture_output({
-    mr_b =  suppressWarnings(
-      stan(stan_model, data = data, chains = 4, iter = iter,
-               seed = 4711, refresh = FALSE))
-  })
-  expect_is(mr_b, "stanfit")
 
-  stan_model = "../../exec/linexp_gastro_1c.stan"
-  mr_c = stan(stan_model, data = data, chains = 4, iter = iter,
-              seed = 4711, refresh = FALSE)
-
-  stan_model = "../../exec/linexp_gastro_1d.stan"
-  mr_d = stan(stan_model, data = data, chains = 4, iter = iter,
-              seed = 4711, refresh = FALSE)
-
-  m_d = get_posterior_mean(mr_d)[,5];
-  m_c = get_posterior_mean(mr_c)[,5];
-  m_b = get_posterior_mean(mr_b)[,5];
-  cbind(m_b, m_c, m_c)
-
-  sum(get_elapsed_time(mr_b))
-  sum(get_elapsed_time(mr_c))
-  sum(get_elapsed_time(mr_d))
-})
-
-
-run_precompiled_model = function(model){
+run_precompiled_model = function(model, iter = 500){
   cat("\nTesting ", model,"\n") # Generate output for travis
   mod = gastempt:::stanmodels[[model]]
   testthat::expect_s4_class(mod, "stanmodel")
@@ -72,14 +38,15 @@ run_precompiled_model = function(model){
   data$student_df = 5
   cap = capture_output({
     fit = suppressWarnings(
-      rstan::sampling(mod, data = data, chains = 2, iter = 500,
+      rstan::sampling(mod, data = data, chains = 2, iter = iter,
                       refresh = -1, verbose = FALSE))
   })
   expect_is(fit, "stanfit")
 }
 
 test_that("Running precompiled models linexp _1x directly returns valid result", {
-  run_precompiled_model("linexp_gastro_1b")
+  iter = ifelse(identical(Sys.getenv("NOT_CRAN"), "true"), 500, 100)
+  run_precompiled_model("linexp_gastro_1b", iter = iter)
 })
 
 test_that("Running precompiled models linexp _1x directly returns valid result (", {
@@ -107,6 +74,7 @@ test_that("Running precompiled powexp models directly returns valid result", {
 })
 
 test_that("Running stan_gastempt fit with default parameters returns valid result", {
+  skip_on_cran()
   d = simulate_gastempt(n_records = 6, seed = 471)
   v0_d = d$rec$v0
   chains = 2 # Problems with more chains on travis
@@ -169,5 +137,37 @@ test_that("Running stan_gastempt with many missing data returns valid result", {
   expect_lt(sqrt(var(v0_d - v0_f)), 8)
   expect_true(all(c("sigma", "mu_kappa", "sigma_kappa", "lp") %in%
                     names(attributes(ret$coef))))
+})
+
+test_that("Direct use of sample model returns valid results", {
+  skip("Slow. Only use on errors in other Stan functions.")
+  data = gastempt_data()
+  stan_model = "../../exec/linexp_gastro_2b.stan"
+  expect_true(file.exists(stan_model))
+  rstan_options(auto_write = TRUE)
+  iter = 500
+  cap = capture_output({
+    mr_b =  suppressWarnings(
+      stan(stan_model, data = data, chains = 4, iter = iter,
+           seed = 4711, refresh = FALSE))
+  })
+  expect_is(mr_b, "stanfit")
+
+  stan_model = "../../exec/linexp_gastro_1c.stan"
+  mr_c = stan(stan_model, data = data, chains = 4, iter = iter,
+              seed = 4711, refresh = FALSE)
+
+  stan_model = "../../exec/linexp_gastro_1d.stan"
+  mr_d = stan(stan_model, data = data, chains = 4, iter = iter,
+              seed = 4711, refresh = FALSE)
+
+  m_d = get_posterior_mean(mr_d)[,5];
+  m_c = get_posterior_mean(mr_c)[,5];
+  m_b = get_posterior_mean(mr_b)[,5];
+  cbind(m_b, m_c, m_c)
+
+  sum(get_elapsed_time(mr_b))
+  sum(get_elapsed_time(mr_c))
+  sum(get_elapsed_time(mr_d))
 })
 
