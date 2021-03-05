6fed5485c9c079ae36de7fdeafa41c2a4fb649f8
Dieter <dieter.menne@menne-biomed.de>
2021-03-05 17:37:36 +0100

    Deleted zzz.R

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onLoad <- function(libname, pkgname) {  # nocov start
-  modules <- paste0("stan_fit4", names(stanmodels), "_mod")
-  for (m in modules) Rcpp::loadModule(m, what = TRUE)
-} # nocov end
